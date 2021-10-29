af7cf035c60d51b1381086c7002e0f7939eaf426
Ariele Gibson <arielegibson@newhuaweiapstudent-10-58-195-220.ncl.ac.uk>
2021-10-29 11:25:35 +0100

    base world

---
+++ Go to the Park/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+
+    [SerializeField] private float jumpHeight;
+
+    private CharacterController controller;
+    private Animator anim;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
+        // if grounded, stop applying gravity
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+
+        float moveZ = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(0, 0, moveZ);
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        if (isGrounded)
+        {
+            if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.UpArrow))
+            {
+                Walk();
+            }
+            if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.RightShift))
+            {
+                Run();
+            }
+            if (moveDirection != Vector3.zero) 
+            {
+                Idle();
+            }
+            
+            moveDirection *= moveSpeed;
+        }
+        else {
+            // apply gravity to player
+            velocity.y = gravity * Time.deltaTime;
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Idle()
+    {
+        anim.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
+    }
+
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+        anim.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+    }
+
+    private void Run()
+    {
+        moveSpeed = runSpeed;
+        anim.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
+    }
+}
